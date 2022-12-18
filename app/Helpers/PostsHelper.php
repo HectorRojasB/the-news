795c7ea19215e56d70540776ef43b13dcd575a09
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2022-12-17 20:56:21 -0600

    Refactor paginator

---
+++ app/Helpers/PostsHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Helpers;
+
+use League\Fractal\Scope;
+use League\Fractal\Manager;
+use App\Transformers\PostTransformer;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+
+class PostsHelper
+{
+    private $fractal;
+    
+    function __construct(Manager $fractal)
+    {
+        $this->fractal = $fractal;
+    }
+
+    public function paginator($postResults) : Scope
+    {
+        $posts = new Collection($postResults, new PostTransformer());
+        $posts->setPaginator(new IlluminatePaginatorAdapter($postResults));
+
+        return $this->fractal->createData($posts);
+    }
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,34 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use League\Fractal\Manager;
+use App\Helpers\PostsHelper;
 use Illuminate\Http\JsonResponse;
 use App\Transformers\PostTransformer;
 use App\Http\Requests\StorePostRequest;
-use League\Fractal\Resource\Collection;
-use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class PostController extends Controller
 {
-    private $fractal;
+    private $helper;
     private $postTransformer;
 
-    function __construct(Manager $fractal, PostTransformer $postTransformer)
+    function __construct(PostsHelper $helper, PostTransformer $postTransformer)
     {
-        $this->fractal = $fractal;
+        $this->helper = $helper;
         $this->postTransformer = $postTransformer;
 
     }
     public function index(): ?array
     {
         $postsPaginator = Post::orderBy('created_at', 'desc')->paginate(10);
-
-        $posts = new Collection($postsPaginator, new PostTransformer());
-        $posts->setPaginator(new IlluminatePaginatorAdapter($postsPaginator));
-
-        $posts = $this->fractal->createData($posts);
-
-        return $posts->toArray();
+        return $this->helper->paginator($postsPaginator)->toArray();
     }
 
     public function store(StorePostRequest $request) : JsonResponse
@@ -70,12 +62,6 @@ class PostController extends Controller
     public function search($param): ?array
     {
         $results = (new Post())->search($param);
-
-        $posts = new Collection($results, new PostTransformer());
-        $posts->setPaginator(new IlluminatePaginatorAdapter($results));
-
-        $posts = $this->fractal->createData($posts);
-
-        return $posts->toArray();
+        return $this->helper->paginator($results)->toArray();
     }
 }
